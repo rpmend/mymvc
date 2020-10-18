e5a343a3493783b0eff08f4334c70c43a09cac73
Ricardo Mendes <rpmendes@outlook.com.br>
2020-10-18 06:33:25 -0300

    adds database class

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,11 @@
 <?php
 
+// Database params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mymvc');
+
 // App root
 define('APPROOT', dirname(dirname(__FILE__)));
 
--- app/libraries/database.php
+++ app/libraries/database.php
@@ -1,3 +1,33 @@
 <?php
 
 // PDO class
+
+class Database
+{
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __construct() {
+        // SET DSN
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        // Create PDO instance
+
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+}

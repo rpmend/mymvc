c12716b588240f8f91692a0beae2767501b2f613
Ricardo Mendes <rpmendes@outlook.com.br>
2020-10-17 23:28:36 -0300

    implements base controller

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,12 +1,13 @@
 <?php
 
-class Pages
+class Pages extends Controller
 {
     public function __construct() {
     }
 
     public function index()
     {
+        $this->view('helo');
     }
 
     public function about($id)
--- app/libraries/controller.php
+++ app/libraries/controller.php
@@ -1,2 +1,32 @@
 <?php
 
+/*
+ * Base Controller
+ * Loads the models and views
+ */
+
+class Controller
+{
+    public function model($model)
+    // Load model
+    {
+        // Require model file
+        require_once '../app/models/' . $model . '.php';
+
+        // Instantiate the model
+        return new $model();
+    }
+
+    // Load view
+    public function view($view, $data = [])
+    {
+        // Check for the view file
+        if (file_exists('..app/view/' . $view . '.php')) {
+            // Require view file
+            require_once '../app/views/' . $view . '.php';
+        }else {
+            // View does not exists
+            die('View does not exists.');
+        }
+    }
+}

6944a515ccf249d0199c1b3cd883ea677dc4d7a2
Ricardo Mendes <rpmendes@outlook.com.br>
2020-10-18 15:15:13 -0300

    wraps up base framework

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,8 +1,4 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <h1><?php echo $data['title']; ?></h1>
-<ul>
-    <?php foreach($data['posts'] as $post) : ?>
-    <li><?php echo $post->title; ?></li>
-    <?php endforeach; ?>
-</ul>
+<p>Welcome to the the MyMVC PHP framework created by rpmend.</p>
 <?php require APPROOT . '/views/inc/footer.php'; ?>

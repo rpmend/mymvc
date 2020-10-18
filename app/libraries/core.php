5c4c8958e5df912c4a734b649e80e0489c89603e
Ricardo Mendes <rpmendes@outlook.com.br>
2020-10-17 23:17:38 -0300

    implements core.php

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -15,11 +15,51 @@ class Core
 
     public function __construct()
     {
-        $this->getUrl();
+        $url = $this->getUrl();
+
+        // This line is to prevent array offset error in php 7.4
+        if (!isset($url[0])) {
+            $url[0] = 'pages';
+        }
+
+        // Look in controllers for first value
+        if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            // If exiss, set as controller
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 index
+            unset($url[0]);
+        }
+
+        // Require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        // Instantiate controller class
+        $this->currentController = new $this->currentController;
+
+        // Check for second part of url        
+        if (isset($url[1])) {
+            // Check if method exists in controller
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethod = $url[1];
+                // Unset 1 index
+                unset($url[1]);
+            }
+        }
+
+        // Get params
+        $this->params = $url ? array_values($url) : [];
+
+        // Call the callback with array of params
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     public function getUrl()
     {
-        echo $_GET['url'];
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }

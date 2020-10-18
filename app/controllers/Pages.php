dfb4baae377dab26b307a2b4941909dc82e40474
Ricardo Mendes <rpmendes@outlook.com.br>
2020-10-18 15:06:27 -0300

    implements pdo methods for testing

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,25 +1,29 @@
 <?php
-
 class Pages extends Controller
 {
-    public function __construct() {
+    public function __construct()
+    {
         $this->postModel = $this->model('Post');
     }
 
     public function index()
     {
+        $posts = $this->postModel->getPosts();
+
         $data = [
-            'title' => 'welcome'
+            'title' => 'Welcome',
+            'posts' => $posts
         ];
-        
+
         $this->view('pages/index', $data);
     }
 
     public function about()
     {
         $data = [
-            'title' => 'about'
+            'title' => 'About Us'
         ];
+
         $this->view('pages/about', $data);
     }
 }
--- app/models/Post.php
+++ app/models/Post.php
@@ -1,10 +1,17 @@
 <?php
-
 class Post
 {
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = new Database;
+    }
+
+    public function getPosts()
+    {
+        $this->db->query("SELECT * FROM posts");
+
+        return $this->db->resultSet();
     }
 }
